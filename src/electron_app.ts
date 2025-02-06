2fa114f0c9dd6821e2e493645d42a2748337852d
Joey Zhao <joeyjoeyze@gmail.com>
2025-02-06 13:01:35 -0500

    move to electron v31, fix bugs, app runs again

--- src/electron_app.ts
+++ src/electron_app.ts
@@ -1,29 +1,38 @@
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
 
 export class ElectronApp {
-    private win: BrowserWindow;
+  private win: BrowserWindow;
 
-    public start() {
-        app.on('ready', () => this.createWindow());
+  public start() {
+    app.on("ready", () => this.createWindow());
 
-        app.on('window-all-closed', () => {
-            app.quit();
-        });
-    }
+    app.on("window-all-closed", () => {
+      app.quit();
+    });
+  }
 
-    private createWindow() {
-        this.win = new BrowserWindow({width: 1000, height: 600});
+  private createWindow() {
+    this.win = new BrowserWindow({
+      width: 1000,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    });
 
-        this.win.loadURL(url.format({
-            pathname: path.join(__dirname, 'view', 'index.html'),
-            protocol: 'file:',
-            slashes: true
-        }));
+    this.win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "view", "index.html"),
+        protocol: "file:",
+        slashes: true,
+      }),
+    );
 
-        this.win.on('closed', () => {
-            this.win = null;
-        });
-    }
+    this.win.on("closed", () => {
+      this.win = null;
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import * as config from './config';
-import { MarklinIO } from './marklin/marklin_io';
-import { MarklinController } from './marklin/marklin_controller';
-import { TrackDrift } from './marklin/setup/track_drift';
-import { TrackA } from './marklin/setup/track_a';
-import { ElectronApp } from './electron_app';
-import { ipcMain, Event } from 'electron';
-import { ITickPayload } from './util/tick_payload';
+import * as config from "./config";
+import { MarklinIO } from "./marklin/marklin_io";
+import { MarklinController } from "./marklin/marklin_controller";
+import { TrackDrift } from "./marklin/setup/track_drift";
+import { TrackA } from "./marklin/setup/track_a";
+import { ElectronApp } from "./electron_app";
+import { ipcMain, IpcMainEvent } from "electron";
+import { ITickPayload } from "./util/tick_payload";
 
 const io = new MarklinIO();
 io.listenTCP(config.SOCKET_PORT);
@@ -23,12 +23,12 @@ eApp.start();
 const interval = 1000 / config.TICK_RATE;
 setInterval(() => controller.tick(interval), interval);
 
-ipcMain.on('getTickDelta', (e: Event) => {
-    const payload: ITickPayload = controller.getTick(true);
-    e.returnValue = JSON.stringify(payload);
+ipcMain.on("getTickDelta", (e: IpcMainEvent) => {
+  const payload: ITickPayload = controller.getTick(true);
+  e.returnValue = JSON.stringify(payload);
 });
 
-ipcMain.on('getTickFull', (e: Event) => {
-    const payload: ITickPayload = controller.getTick(false);
-    e.returnValue = JSON.stringify(payload);
+ipcMain.on("getTickFull", (e: IpcMainEvent) => {
+  const payload: ITickPayload = controller.getTick(false);
+  e.returnValue = JSON.stringify(payload);
 });
